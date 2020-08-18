fa30def060f09cf1b76a3bb5b9f05ea201f8cb22
Christian Lussier <christianlussier@Christians-MacBook-Pro.local>
2020-08-17 21:02:23 -0400

    Minor fixes

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
-"""WIP GOING TO BE SOME NASTY CODE FOR NOW, IN ONE FILE."""
+"""WIP -- GOING TO BE SOME NASTY CODE FOR NOW, IN ONE FILE."""
 
 def get_games():
     """Gets the user's chosen games for parlays."""
     num_of_games = int(input("* Enter number of games: "))
+    game_list = []
     for game in range(num_of_games):
         print("--------------")
         team1 = input("*- Enter Team 1 Name: ")
         team1_odds = input("*-- Enter Team 1 Odds: ")
         print("-----")
         team2 = input("*- Enter Team 2 Name: ")
-        team2_odds = input("*-- Enter Team 1 Odds: ")
+        team2_odds = input("*-- Enter Team 2 Odds: ")
         # store in object here
     print("Finished entering information!")
 
