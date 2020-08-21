d04c8602974a0471ce1412d08029cc6289ec1498
Christian Lussier <lussierc@allegheny.edu>
2020-08-21 01:53:22 -0400

    Change basic data storage

--- main.py
+++ main.py
@@ -6,21 +6,28 @@ def get_games():
     data_dict={}
     num_of_games = int(input("* Enter number of games: "))
     game_list = []
+    teams1_list = []
+    teams2_list = []
     for game in range(num_of_games):
         game_info = []
         print("--------------")
         team1 = input("*- Enter Team 1 Name: ")
         team1_odds = input("*-- Enter Team 1 Odds: ")
         team1_dict = {team1:team1_odds}
-        print(team1_dict)
+        teams1_list.append(team1)
+        data_dict.update(team1_dict)
+
         print("-----")
+
         team2 = input("*- Enter Team 2 Name: ")
         team2_odds = input("*-- Enter Team 2 Odds: ")
         team2_dict = {team2:team2_odds}
-        game_info.append(team1_dict)
-        game_info.append(team2_dict)
-        game_list.append(game_info)
-        print(game_list)
+        teams2_list.append(team2)
+        data_dict.update(team2_dict)
+        
+        print(data_dict)
+
+
 
     # store in object here
     print("Finished entering information!")
